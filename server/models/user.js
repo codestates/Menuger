4badf905e57dcb9935e752f321ed39e8b451c726
Soujiro-a <qmaecx@naver.com>
2021-09-17 16:56:35 +0900

    [Server] Feat: 회원가입 구현

--- server/models/user.js
+++ server/models/user.js
@@ -4,10 +4,14 @@ const {
   Types: { ObjectId },
 } = require('mongoose');
 
+const bcrypt = require('bcrypt');
+const e = require('express');
+let saltRound = 3; //salt를 돌리는 횟수
+
 const userSchema = new Schema(
   {
-    type: { type: String, default: 'user' },
-    email: { type: String, required: true },
+    type: { type: String, default: 'user', enum: ['user', 'admin'] },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     nickname: { type: String, required: true, unique: true },
     image_url: String,
@@ -15,6 +19,27 @@ const userSchema = new Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRound, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, (err, hashedPassword) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hashedPassword;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 const User = model('user', userSchema);
 

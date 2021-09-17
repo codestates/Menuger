544fb32f4cf1cefd778d55f130e13084ba487430
Soujiro-a <qmaecx@naver.com>
2021-09-17 11:07:59 +0900

    [Server] Fix: start.sh 파라미터 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/Menuger/server
 
+export MONGO_URL=$(aws ssm get-parameters --region ap-northeast-2 --names MONGO_URL --query Parameters[0].Value | sed 's/"//g')
+export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value | sed 's/"//g')
+
 authbind --deep pm2 start app.js

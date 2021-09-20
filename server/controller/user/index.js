2cc68670c688cc7c54dbe94fe415f1e42b54b964
Soujiro-a <qmaecx@naver.com>
2021-09-20 19:41:12 +0900

    [Server] Fix: deleteaccount -> deleteAccount 변수 수정

--- server/controller/user/index.js
+++ server/controller/user/index.js
@@ -2,5 +2,5 @@ module.exports = {
   signup: require('./signup'),
   signin: require('./signin'),
   signout: require('./signout'),
-  deleteaccount: require('./deleteaccount'),
+  deleteAccount: require('./deleteAccount'),
 };

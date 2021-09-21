444f1f871d6dab024fb226d190bad55a538de3f6
Soujiro-a <qmaecx@naver.com>
2021-09-21 10:36:32 +0900

    [Server] Feat: 유저정보 수정 구현

--- server/controller/user/info.js
+++ server/controller/user/info.js
@@ -1,24 +1,64 @@
 const { User } = require('../../models/user');
+const { verifyAccessToken } = require('../utils/jwt');
+const {
+  Types: { ObjectId },
+} = require('mongoose');
 
 module.exports = {
   get: (req, res) => {
-    const { nickname } = req.params;
-    if (!nickname) {
-      res.status(400).send({ message: '닉네임을 입력받지 않았습니다.' });
-    }
-
-    User.findOne({ nickname }, (err, user) => {
-      if (err) {
-        return res.status(400).send(err);
+    try {
+      const { nickname } = req.params;
+      if (!nickname) {
+        res.status(400).send({ message: '닉네임을 입력받지 않았습니다.' });
       }
+
+      User.findOne({ nickname }, (err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+
+        if (!user) {
+          return res.status(400).send({ message: '존재하지 않는 유저입니다.' });
+        }
+
+        const { email, nickname, image_url = '' } = user;
+
+        res.status(200).send({ nickname, email, image_url });
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send({ message: err.message });
+    }
+  },
+  patch: async (req, res) => {
+    try {
+      const { payload } = verifyAccessToken(req.cookies.accessToken);
+      if (!payload) {
+        return res.status(400).send({ message: '유효하지 않은 접근입니다.' });
+      }
+
+      const user = await User.findById(ObjectId(payload));
 
       if (!user) {
-        return res.status(400).send({ message: '존재하지 않는 유저입니다.' });
+        return res.status(400).send({ message: '인증되지 않은 유저입니다.' });
       }
 
-      const { email, nickname, image_url = '' } = user;
+      const {
+        password = user.password,
+        image_url = user.image_url,
+        nickname = user.nickname,
+      } = req.body;
 
-      res.status(200).send({ nickname, email, image_url });
-    });
+      user.password = password;
+      user.image_url = image_url;
+      user.nickname = nickname;
+
+      await user.save();
+
+      return res.status(200).send({ message: 'modify userinfo success' });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send({ message: err.message });
+    }
   },
 };

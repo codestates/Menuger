7f0013280c64231ce7ab107f0d9518da05311ea2
jc <jotace422@gmail.com>
2021-09-23 01:17:54 +0900

    [Client] Feat: toastReducer를 rootReducer에 추가

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 //import module
 import ex_counter from './ex_counter';
 import dietColumnContainer from './dietColumnContainer';
+import toastReducer from './toast';
 
 const rootReducer = combineReducers({
   //module 등록
   ex_counter,
   dietColumnContainer,
+  toastReducer,
 });
 
 export default rootReducer;

8f4ddd0d5083b4435162e65990464885abeaf617
jc <jotace422@gmail.com>
2021-09-23 03:47:47 +0900

    [Client] Feat: default mode, default delay 추가

--- client/src/hooks/toast/useToast.js
+++ client/src/hooks/toast/useToast.js
@@ -1,8 +1,10 @@
 import { useSelector } from 'react-redux';
 
+import { TOAST_DELAY } from '../../components/common/ToastPortal/constants';
+
 const useToast = () => {
   const { toastRef } = useSelector(state => state.toastReducer);
-  const addMessage = ({ mode, message, delay }) => {
+  const addMessage = ({ mode = 'success', message, delay = TOAST_DELAY }) => {
     toastRef.current.addMessage({
       mode,
       message,

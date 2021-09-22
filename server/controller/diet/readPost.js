b28ae4542f7bfa1bb520441172966dcb8cabadc5
Soujiro-a <qmaecx@naver.com>
2021-09-22 20:30:22 +0900

    [Server] Fix: 레시피/식단 조회 populate 자식 추가 부분 삭제

--- server/controller/diet/readPost.js
+++ server/controller/diet/readPost.js
@@ -9,7 +9,7 @@ module.exports = {
         res.status(400).send({ message: '해당 id는 유효하지 않습니다.' });
       }
 
-      const diet = await Diet.findOne({ _id: id }).populate([{ path: 'user' }]);
+      const diet = await Diet.findOne({ _id: id });
 
       return res.status(200).send({ diet });
     } catch (err) {
--- server/controller/recipe/readPost.js
+++ server/controller/recipe/readPost.js
@@ -9,7 +9,7 @@ module.exports = {
         res.status(400).send({ message: '해당 id는 유효하지 않습니다.' });
       }
 
-      const recipe = await Recipe.findOne({ _id: id }).populate([{ path: 'user' }]);
+      const recipe = await Recipe.findOne({ _id: id });
 
       return res.status(200).send({ recipe });
     } catch (err) {

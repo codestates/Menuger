788bdcb61a6742c47af33b74620f98c2d7ea935b
jc <jotace422@gmail.com>
2021-09-23 00:55:43 +0900

    [Client] Chore: toast 컴포넌트에서 사용되는 모든 상수를 관리하는 파일 추가

---
+++ client/src/components/common/ToastPortal/constants.js
@@ -0,0 +1,6 @@
+export const TOAST_DELAY = 2000;
+export const SLIDE_DELAY = 2000;
+export const KEYFRAME_END = 100;
+export const BOX_TOP = 10;
+export const BOX_RIGHT = 10;
+export const SEC_IN_MS = 1000;

21283cb92e5fe7b19e2f0e4f281db4a27c762b9e
jc <jotace422@gmail.com>
2021-09-25 22:26:11 +0900

    [Client] Refactor: 구독 버튼 및 기능을 재사용 가능하도록 custom hook으로 분리

---
+++ client/src/hooks/useSubscribe.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+import SubscribeButton from '../components/common/buttons/SubscribeButton';
+
+const useSubscribe = (postId, isSubscribed, fontSize = 12) => {
+  const [subscribed, setSubscribed] = useState(isSubscribed);
+  const [loading, setLoading] = useState(false);
+
+  // !REMOVE: sample api call
+  const API_CALL_SUB_OR_UNSUB = () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (subscribed) {
+          resolve('구독이 취소되었습니다.');
+        } else {
+          resolve('구독이 신청되었습니다.');
+        }
+      }, 2000);
+    });
+  };
+
+  const toggle = async () => {
+    setLoading(true);
+
+    //TODO: replace with axios sub/unsub api call
+    const { message } = await API_CALL_SUB_OR_UNSUB();
+
+    setSubscribed(subState => !subState);
+    setLoading(false);
+
+    //TODO: add custom Toast
+    //ex) addMessage({ message });
+  };
+
+  return () => (
+    <SubscribeButton
+      subscribed={subscribed}
+      toggle={toggle}
+      loading={loading}
+      fontSize={fontSize}
+    />
+  );
+};
+
+export default useSubscribe;

844715cc0a90609b396af704ef3808184c2f9218
jc <jotace422@gmail.com>
2021-09-27 04:38:05 +0900

    [Client] Feat: 무한스크롤링 구현에 필요한 ScrollEnd 엘리먼트를 CardList 컴포넌트의 하단에 추가

--- client/src/components/common/cards/CardList.js
+++ client/src/components/common/cards/CardList.js
@@ -1,6 +1,9 @@
+import { forwardRef } from 'react';
 import styled from 'styled-components';
 
 import CardItem from './CardItem';
+import svgToComponent from '../../../utils/svg';
+import scrollToTop from '../../../utils/scroll';
 
 const Wrapper = styled.div`
   overflow-x: hidden;
@@ -14,27 +17,41 @@ const CardListContainer = styled.ul`
   }
 `;
 
-const CardList = ({ cards }) => {
+const ScrollEnd = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CardList = forwardRef(({ cards, hasNext }, fetchMoreRef) => {
+  const arrowDownConfig = { svgName: 'arrowDown' };
+  const arrowUpConfig = { svgName: 'arrowUp', props: { onClick: scrollToTop, cursor: 'pointer' } };
+
   return (
-    <Wrapper>
-      <CardListContainer>
-        {cards.map(card => (
-          <CardItem
-            key={card.postId}
-            postId={card.postId}
-            postType={card.postType}
-            title={card.title}
-            subscribed={card.subscribed}
-            img={card.img}
-            imgFileName={card.imgFileName}
-            userInfo={card.userInfo}
-            postInfo={card.postInfo}
-            tagInfo={card.tagInfo}
-          />
-        ))}
-      </CardListContainer>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <CardListContainer>
+          {cards.map(card => (
+            <CardItem
+              key={card.postId}
+              postId={card.postId}
+              postType={card.postType}
+              title={card.title}
+              subscribed={card.subscribed}
+              img={card.img}
+              imgFileName={card.imgFileName}
+              userInfo={card.userInfo}
+              postInfo={card.postInfo}
+              tagInfo={card.tagInfo}
+            />
+          ))}
+        </CardListContainer>
+      </Wrapper>
+      <ScrollEnd ref={fetchMoreRef}>
+        {hasNext && cards.length > 0 && svgToComponent(arrowDownConfig)}
+        {!hasNext && svgToComponent(arrowUpConfig)}
+      </ScrollEnd>
+    </>
   );
-};
+});
 
 export default CardList;

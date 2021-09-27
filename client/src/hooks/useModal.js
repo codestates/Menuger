3ef2141f507dcab79bf8b0a904bdac07d16dfed7
jc <jotace422@gmail.com>
2021-09-27 16:10:26 +0900

    [Client] Fix: props로 전달된 색상, 투명도, 너비, 높이에 따라 modal을 style할 수 있도록 수정

--- client/src/hooks/useModal.js
+++ client/src/hooks/useModal.js
@@ -2,14 +2,22 @@ import { useState } from 'react';
 
 import Modal from '../components/common/modal/Modal';
 
-const useModal = () => {
+const useModal = (
+  style = { overlayColor: 'black', overlayOpacity: 0.2, width: 100, height: 90 },
+) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const showModal = () => setIsVisible(true);
   const hideModal = () => setIsVisible(false);
 
   const ModalContainer = ({ children }) => (
-    <>{isVisible && <Modal hideModal={hideModal}>{children}</Modal>}</>
+    <>
+      {isVisible && (
+        <Modal hideModal={hideModal} style={style}>
+          {children}
+        </Modal>
+      )}
+    </>
   );
 
   return { isVisible, showModal, hideModal, ModalContainer };

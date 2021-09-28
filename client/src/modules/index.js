25fe1b072bc168e852e3ad6e7f0ca53996c1fe6b
jc <jotace422@gmail.com>
2021-09-29 04:43:27 +0900

    [Client] Feat: 로그인한 유저정보를 redux 상태 관리할 수 있도록 reducer 등록

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -4,12 +4,14 @@ import { combineReducers } from 'redux';
 import ex_counter from './ex_counter';
 import dietColumnContainer from './dietColumnContainer';
 import toastReducer from './toast';
+import userReducer from './user';
 
 const rootReducer = combineReducers({
   //module 등록
   ex_counter,
   dietColumnContainer,
   toastReducer,
+  userReducer,
 });
 
 export default rootReducer;

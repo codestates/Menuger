46b929d9d07855fa79bd5b09adedf4d3a5bc6746
jc <jotace422@gmail.com>
2021-10-04 02:11:05 +0900

    [Client] Fix: 게시글 정렬 관련 데이터 추가

--- client/src/utils/sort.js
+++ client/src/utils/sort.js
@@ -1,6 +1,7 @@
+export const sortOptions = ['dd', 'p', 'da'];
 export const sortMenus = ['최신순', '인기순', '등록순'];
 export const sortOptionMapper = {
-  최신순: -1,
-  인기순: 0,
-  등록순: 1,
+  dd: -1,
+  p: 0,
+  da: 1,
 };

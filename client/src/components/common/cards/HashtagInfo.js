4d472c2eb16ae86cd7aa0ae7c2c3fd2ac2156349
jc <jotace422@gmail.com>
2021-10-06 18:06:10 +0900

    [Client] Feat: 해시태그 버튼 누를 경우 검색 가능하도록 설정

--- client/src/components/common/cards/HashtagInfo.js
+++ client/src/components/common/cards/HashtagInfo.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import { createBrowserHistory } from 'history';
 
 const Hashtag = styled.div`
+  display: inline-block;
   color: ${({ color }) => color || '#4a90e2'};
   font-size: ${({ fs }) => fs || '0.7rem'};
+  margin-right: 0.6em;
+  margin-bottom: 0.3em;
+
   @media (max-width: 1200px) {
     font-size: ${({ fs1200 }) => fs1200 || '0.9rem'};
   }
@@ -18,15 +23,35 @@ const Hashtag = styled.div`
   }
 `;
 
-const HashtagInfo = ({ hashtags, style = {} }) => {
+const HashtagInfo = ({ postType, hashtags, style = {} }) => {
   const { fs, fs1200, fs900, fs768, color } = style;
+  const history = createBrowserHistory({ forceRefresh: true });
+
+  const handleHashtagClick = hashtag => {
+    localStorage.setItem('searched', `#${hashtag}`);
+    history.push({
+      pathname: `/${postType}`,
+      search: '?sort=dd',
+      state: { input: `#${hashtag}` },
+    });
+  };
 
   return (
     <>
       {!!hashtags.length && (
         <>
           {hashtags.map((hashtag, idx) => (
-            <Hashtag key={idx} fs={fs} fs1200={fs1200} fs900={fs900} fs768={fs768} color={color}>
+            <Hashtag
+              key={idx}
+              fs={fs}
+              fs1200={fs1200}
+              fs900={fs900}
+              fs768={fs768}
+              color={color}
+              onClick={() => {
+                handleHashtagClick(hashtag);
+              }}
+            >
               #{hashtag}
             </Hashtag>
           ))}

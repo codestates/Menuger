3929d888f9a3bcb6db83768a2d7902f185475716
jc <jotace422@gmail.com>
2021-10-10 13:30:18 +0900

    [Client] Fix: alert 를 통해 진행하도록 수정 + 비밀번호 입력안한 경우 로그아웃 되지 않도록 수정

--- client/src/components/mypage/DeleteMyAccount.js
+++ client/src/components/mypage/DeleteMyAccount.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { useHistory } from 'react-router';
 import { setUserInfo } from '../../modules/user';
+import useToast from '../../hooks/toast/useToast';
 
 import { lighten, darken } from 'polished';
 
@@ -24,8 +25,9 @@ const TextContainer = styled.div`
   margin-top: 3em;
   margin-left: 2em;
   h1 {
-    font-size: 2em;
-    color: ${darken(0.1, '#87e0f3')};
+    font-weight: bold;
+    font-size: 2.5em;
+    color: crimson;
     @media (max-width: 768px) {
       font-size: 1em;
       width: 70%;
@@ -34,6 +36,11 @@ const TextContainer = styled.div`
   span {
     @media (max-width: 768px) {
       font-size: 0.8em;
+    }
+  }
+  &.isDark {
+    & > span {
+      color: white;
     }
   }
 `;
@@ -61,18 +68,23 @@ const FormContainer = styled.div`
   .title {
     font-weight: bold;
   }
+  &.isDark {
+    color: white;
+  }
 `;
 
 const ButtonContainer = styled.div`
   margin-top: 3em;
   button {
-    width: 6em;
-    height: 3em;
+    padding: 0.8em 1.5em;
     border: none;
-    background-color: ${darken(0.1, '#87e0f3')};
+    background-color: crimson;
     color: white;
+    border-radius: 3px;
+    font-weight: bold;
     :hover {
-      background-color: ${lighten(0.1, '#87e0f3')};
+      background-color: ${lighten(0.1, 'crimson')};
+      cursor: pointer;
     }
   }
 `;
@@ -82,6 +94,8 @@ const DeleteMyAccount = () => {
   const userInfo = useSelector(state => state.user);
   const dispatch = useDispatch();
   const history = useHistory();
+  const addMessage = useToast();
+  const { isDarkMode } = useSelector(state => state.theme);
 
   const user = {
     email: '',
@@ -92,28 +106,37 @@ const DeleteMyAccount = () => {
   };
 
   const deleteAccount = async () => {
-    try {
-      await axios
-        .delete(`${process.env.REACT_APP_ENDPOINT_URL}/users`, {
+    const withdraw = window.confirm('회원 탈퇴를 진행하겠습니다');
+    if (withdraw) {
+      try {
+        const {
+          data: { message },
+          status,
+        } = await axios.delete(`${process.env.REACT_APP_ENDPOINT_URL}/users`, {
           data: { password },
           withCredentials: true,
-        })
-        .then(dispatch(setUserInfo(user)))
-        .then(history.push('/'));
-    } catch (err) {
-      console.log(err);
+        });
+
+        if (status === 200) {
+          addMessage({ message });
+          dispatch(setUserInfo(user));
+          history.push('/');
+        }
+      } catch (err) {
+        addMessage({ mode: 'error', message: err.response.data.message });
+      }
     }
   };
 
   return (
     <Wrapper>
-      <TextContainer>
+      <TextContainer className={isDarkMode && 'isDark'}>
         <h1>메뉴저에서 회원탈퇴하시겠습니까?</h1>
         <br></br>
-        <span>-회원 정보 확인 후 회원탈퇴가 가능합니다.</span>
-        <span>-입력하신 정보는 회원 탈퇴 이외의 목적으로 사용되지 않습니다.</span>
+        <span>• 회원 정보 확인 후 회원탈퇴가 가능합니다.</span>
+        <span>• 입력하신 정보는 회원 탈퇴 이외의 목적으로 사용되지 않습니다.</span>
       </TextContainer>
-      <FormContainer>
+      <FormContainer className={isDarkMode && 'isDark'}>
         <div className="title">
           <span>닉네임</span>
           <span>비밀번호</span>

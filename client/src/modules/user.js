51966b74c2b77373fb300bc825b1244c8a43f838
jc <jotace422@gmail.com>
2021-10-21 05:59:55 +0900

    [Client] Fix: 로그아웃시 유저정보 초기화 할 수 있도록 수정

--- client/src/modules/user.js
+++ client/src/modules/user.js
@@ -1,8 +1,10 @@
 //Action Type
 const SET_USERINFO = 'user/SET_USERINFO';
+const RESET_USERINFO = 'user/RESET_USERINFO';
 
 //Action Creator
 export const setUserInfo = userInfo => ({ type: SET_USERINFO, payload: userInfo });
+export const resetUserInfo = () => ({ type: RESET_USERINFO });
 
 //init
 const initialState = {
@@ -21,6 +23,8 @@ const userReducer = (state = initialState, action) => {
         ...state,
         ...action.payload,
       };
+    case RESET_USERINFO:
+      return initialState;
     default:
       return state;
   }
